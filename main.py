51e748e38fbf0e17135c80647e7802797fd3ff8f
ASWINI A <113587646+aswini826@users.noreply.github.com>
2023-05-17 10:49:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ from routers import auth, users
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:4200"
+]
+
+app.add_middleware (
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
+
 models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
