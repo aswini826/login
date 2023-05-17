23700665f2fa7d9817b1c61daac6e8f5fb956e6f
ASWINI A <113587646+aswini826@users.noreply.github.com>
2023-05-17 10:03:03 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:ashu826@localhost/loginPageDatabase'
+SQLALCHEMY_DATABASE_URL = 'postgresql://login_database_user:0QV1dEp7IgF5RS4uVS8VCASxYDhM2fZH@dpg-chi5hi5269vf5qa924a0-a/login_database'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
