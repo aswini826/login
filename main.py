aaea334695a9ac713932ae05e5d82bc2f7a56b8b
ASWINI A <113587646+aswini826@users.noreply.github.com>
2023-05-17 11:01:53 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import models
 from database import engine
-from routers import auth, users
+from routers import auth
 
 app = FastAPI()
 
@@ -9,13 +10,14 @@ origins = [
     "http://localhost:4200"
 ]
 
-app.add_middleware (
+app.add_middleware(
   CORSMiddleware,
   allow_origins=origins,
   allow_credentials=True,
   allow_methods=["*"],
   allow_headers=["*"],
 )
+
 
 models.Base.metadata.create_all(bind=engine)
 
