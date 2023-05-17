ccfc891c159230a49024cfaeaf31af9f870ba8e0
ASWINI A <113587646+aswini826@users.noreply.github.com>
2023-05-17 12:01:39 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = 'postgresql://login_database_user:0QV1dEp7IgF5RS4uVS8VCASxYDhM2fZH@dpg-chi5hi5269vf5qa924a0-a/login_database'
 
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
