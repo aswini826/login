580e4c5114f02c5f42ca2a65968e92531f1f9cda
ASWINI A <113587646+aswini826@users.noreply.github.com>
2023-05-19 15:52:59 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://login_database_user:0QV1dEp7IgF5RS4uVS8VCASxYDhM2fZH@dpg-chi5hi5269vf5qa924a0-a.oregon-postgres.render.com/login_database'
-
+SQLALCHEMY_DATABASE_URL = 'postgresql://kqglejeh:MOe8y-fWxKG7ZV-7OhUks7BRQTmav47t@drona.db.elephantsql.com/kqglejeh'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
