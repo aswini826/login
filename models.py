061001cbef159bc6ea8e42860e7575fd9b2a217d
ASWINI A <113587646+aswini826@users.noreply.github.com>
2023-05-26 12:36:05 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, Date
 
 
 class Users(Base):
@@ -11,14 +11,6 @@ class Users(Base):
     hashed_password = Column(String)
     register_number = Column(Integer, unique=True)
     phone = Column(String)
-    date_of_birth = Column(String)
+    date_of_birth = Column(Date)
     course = Column(String)
 
-
-class Login(Base):
-    __tablename__ = 'login'
-
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True)
-    hashed_password = Column(String)
-    owner_id = Column(Integer, ForeignKey("users.id"))
