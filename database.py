6c0400167176c1427bc265177376f7c397462a3f
ASWINI A <113587646+aswini826@users.noreply.github.com>
2023-06-12 12:07:23 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://zthubeat:0TzLqHDy7Z-zRMOQ1UGKq-hgwtsR-T0P@jelani.db.elephantsql.com/zthubeat'
+SQLALCHEMY_DATABASE_URL = 'postgresql://meesho_user:oq6atqUb5uZZM1QbpFFQuXQqQWO9nRDs@dpg-ci3arhu7avj2t30374lg-a.oregon-postgres.render.com/meesho'
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
